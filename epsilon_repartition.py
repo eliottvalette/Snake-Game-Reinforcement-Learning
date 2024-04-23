62f6b30d6a241dce866e79a45edbc4b2826bd3b1
Valette Eliott <eliott.valette@hec.edu>
2024-04-23 13:18:48 +0200

    epsilon repartition

---
+++ epsilon_repartition.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot  as plt
+def epsilon_rep(episode):
+    return  max(0.01, 0.99 * (0.98 ** episode))
+
+epsilon_rep = np.vectorize(epsilon_rep)
+
+les_x = np.linspace(0, 500, 10000)
+les_y = epsilon_rep(les_x)
+plt.plot(les_x, les_y, '.')
+plt.show()
